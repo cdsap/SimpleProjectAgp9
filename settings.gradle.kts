6a40c0d4f42debecb4120743173557bbc038c872
Inaki Villar <inaki.seri@gmail.com>
2025-09-29 10:23:57 -0700

    using different apptoach

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -18,7 +18,18 @@ dependencyResolutionManagement {
         mavenCentral()
     }
 }
+plugins {
+    id("com.gradle.develocity") version "4.1"
+    id("com.gradle.common-custom-user-data-gradle-plugin") version "2.2"
 
+}
+
+develocity {
+    server = "https://ge.solutions-team.gradle.com/"
+    allowUntrustedServer = true
+    buildScan {
+        uploadInBackground.set(false)
+    }
+}
 rootProject.name = "My Application"
 include(":app")
- 
