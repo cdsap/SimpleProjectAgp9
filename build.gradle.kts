2445d6add15d08d7d343b7096fb178b83e71fccb
Inaki Villar <inaki.seri@gmail.com>
2025-09-29 10:51:05 -0700

    add

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,11 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     alias(libs.plugins.android.application) apply false
+    id("org.gradle.android.cache-fix") version "3.0.1" apply false
+}
+
+subprojects {
+    plugins.withType<com.android.build.gradle.api.AndroidBasePlugin>() {
+        apply(plugin = "org.gradle.android.cache-fix")
+    }
 }
